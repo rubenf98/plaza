99b2088991aa6552dc6a872a71f108ccf52a6153
Ruben Freitas <joseruben98@hotmail.com>
2020-08-13 21:31:25 +0100

    Fraço seeds

--- database/migrations/2020_03_08_162910_create_fracaos_table.php
+++ database/migrations/2020_03_08_162910_create_fracaos_table.php
@@ -18,8 +18,14 @@ class CreateFracaosTable extends Migration
             $table->integer('fracao_estado_id')->unsigned();
             $table->integer('user_id')->unsigned()->nullable();
             $table->integer('bloco_id')->unsigned();
-            $table->string('nome');
-            $table->integer('area');
+            $table->string('nome')->unique();
+            $table->string('condomino')->nullable();
+            $table->enum('piso', ['R/C', '1º', '2º', '3º', '4º', '5º']);
+            $table->enum('tipo', ['T0', 'T1', 'T2', 'T3', 'T4', 'T5']);
+            $table->double('area', 5, 2);
+            $table->double('permilagem', 4, 2);
+            $table->integer('estacionamento')->unique();
+            $table->integer('arrecadação')->unique();
             $table->timestamps();
 
             $table->foreign('fracao_estado_id')->references('id')->on('fracao_estados')->onDelete('cascade');
