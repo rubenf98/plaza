d9c9ea5e074832afd6653a5fbb62d3d18f995367
Ruben Freitas <joseruben98@hotmail.com>
2020-11-29 18:14:23 +0000

    added html tag to emails

--- resources/views/emails/recoverPassword.blade.php
+++ resources/views/emails/recoverPassword.blade.php
@@ -1,24 +1,31 @@
-<div class="email-container" style="max-width: 600px;width: 90%;margin: auto;display: block;">
-    @include('emails.common.header')
+<!DOCTYPE html>
+<html lang="en">
 
-    <h1 class="title" style="font-weight: normal;">Nova palavra-passe gerada!</h1>
-    <div class="text" style="font-size: 1em;">
-        Foi criada uma nova palavra passe para a conta que possui este email, utilize-a para realizar login.
-        Aconselhamos que altere a palavra-passe fornecida para uma a seu gosto.
+<body>
+    <div class="email-container" style="max-width: 600px;width: 90%;margin: auto;display: block;">
+        @include('emails.common.header')
+
+        <h1 class="title" style="font-weight: normal;">Nova palavra-passe gerada!</h1>
+        <div class="text" style="font-size: 1em;">
+            Foi criada uma nova palavra passe para a conta que possui este email, utilize-a para realizar login.
+            Aconselhamos que altere a palavra-passe fornecida para uma a seu gosto.
+        </div>
+
+        <div class="password-container flex-center"
+            style="display: block;text-align: center;color: #ff7a5c;margin-top: 30px;font-size: 1.2em;font-weight: bold;">
+            {{$password}}
+        </div>
+
+        <div class="warning-container flex-center"
+            style="display: block;text-align: center;color: gray;font-size: .8em;margin-bottom: 30px;">
+            (Caso não tenha efetuado este pedido, contacte a administração)
+        </div>
+
+        @include('emails.common.footer')
     </div>
+</body>
 
-    <div class="password-container flex-center"
-        style="display: block;text-align: center;color: #ff7a5c;margin-top: 30px;font-size: 1.2em;font-weight: bold;">
-        {{$password}}
-    </div>
-
-    <div class="warning-container flex-center"
-        style="display: block;text-align: center;color: gray;font-size: .8em;margin-bottom: 30px;">
-        (Caso não tenha efetuado este pedido, contacte a administração)
-    </div>
-
-    @include('emails.common.footer')
-</div>
+</html>
 
 
 <style>
--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,24 +1,31 @@
-<div class="email-container" style="max-width: 600px;width: 90%;margin: auto;display: block;">
-    @include('emails.common.header')
+<!DOCTYPE html>
+<html lang="en">
 
-    <h1 class="title" style="font-weight: normal;">Bem vindo!</h1>
-    <div class="text" style="font-size: 1em;">
-        Obrigado por se registar na plataforma web Plaza II. Foi-lhe atribuída uma palavra-passe, utilize-a para
-        realizar login. Aconselhamos que altere a palavra-passe fornecida para uma a seu gosto.
+<body>
+    <div class="email-container" style="max-width: 600px;width: 90%;margin: auto;display: block;">
+        @include('emails.common.header')
+
+        <h1 class="title" style="font-weight: normal;">Bem vindo!</h1>
+        <div class="text" style="font-size: 1em;">
+            Obrigado por se registar na plataforma web Plaza II. Foi-lhe atribuída uma palavra-passe, utilize-a para
+            realizar login. Aconselhamos que altere a palavra-passe fornecida para uma a seu gosto.
+        </div>
+
+        <div class="password-container flex-center"
+            style="text-align: center;color: #ff7a5c;margin-top: 30px;font-size: 1.2em;font-weight: bold;display: block">
+            {{$password}}
+        </div>
+
+        <div class="warning-container flex-center"
+            style="text-align: center;color: gray;font-size: .8em;margin-bottom: 30px;">
+            (Caso não tenha efetuado registo nesta plataforma, pode ignorar este email)
+        </div>
+
+        @include('emails.common.footer')
     </div>
+</body>
 
-    <div class="password-container flex-center"
-        style="text-align: center;color: #ff7a5c;margin-top: 30px;font-size: 1.2em;font-weight: bold;display: block">
-        {{$password}}
-    </div>
-
-    <div class="warning-container flex-center"
-        style="text-align: center;color: gray;font-size: .8em;margin-bottom: 30px;">
-        (Caso não tenha efetuado registo nesta plataforma, pode ignorar este email)
-    </div>
-
-    @include('emails.common.footer')
-</div>
+</html>
 
 
 <style>
